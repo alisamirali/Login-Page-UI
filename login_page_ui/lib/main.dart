6b0aa8ee36ff1647b6e5ad411511c95a59531021
alisamirali <62913154+alisamirali@users.noreply.github.com>
2021-06-22 23:41:35 +0200

    first commit

---
+++ login_page_ui/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Login Page",
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
